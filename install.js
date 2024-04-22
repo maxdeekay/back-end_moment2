727351eceb4b07c04ccdc4e2c6ee40cff0119a34
Max <max.assermark@gmail.com>
2024-04-22 17:33:39 +0200

    almost complete project

---
+++ install.js
@@ -0,0 +1,34 @@
+const { Client } = require("pg");
+require("dotenv").config();
+
+// Ansluter till databasen
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+// Felhantering
+client.connect((err) => {
+    if (err) console.log("Fel vid anslutning" + err);
+    else console.log("Ansluten till databasen...");
+});
+
+// Skapar tabell 'workexperience'
+client.query(`
+    DROP TABLE IF EXISTS workexperience;
+    CREATE TABLE workexperience (
+        id          SERIAL PRIMARY KEY,
+        companyname VARCHAR(255),
+        jobtitle    VARCHAR(255),
+        location    VARCHAR(255),
+        startdate   DATE,
+        enddate     DATE,
+        description VARCHAR(255)
+    )
+`);
---
+++ server.js
@@ -0,0 +1,110 @@
+const { Client } = require("pg");
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Database connection
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+client.connect((err) => {
+    (err) ? console.log("Fel vid anslutning" + err) : console.log("Ansluten till databasen...");
+});
+
+// ROUTING
+// Read from database
+app.get("/api/experience", (req, res) => {
+    client.query("SELECT * FROM workexperience ORDER BY id DESC", (err, result) => {
+        if (err) return res.status(500).json({error: "Something went wrong: " + err});
+    
+        if (result.rows.length === 0) {
+            res.status(404).json({message: "No work experiences found."});
+        } else {
+            res.json(result.rows);
+        }
+    });
+});
+
+// Add to database
+app.post("/api/experience", (req, res) => {
+    const {companyname, jobtitle, location, startdate, enddate, description} = req.body;
+
+    // Error handling
+    if (!companyname || !jobtitle || !location || !startdate) {
+        return res.status(400).json({
+            message: "Missing required input.",
+            details: "Company name, job title, location and start date must be included.",
+        });
+    }
+    
+    // Date validation
+    if (isNaN(new Date(startdate).getDate()) || (enddate && isNaN(new Date(enddate).getDate())) || !enddate) {
+        return res.status(400).json({message: "Invalid date format"});
+    } else {
+        // Add work experience
+        client.query(`
+        INSERT INTO workexperience (companyname, jobtitle, location, startdate, enddate, description)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        RETURNING *
+    `, [companyname, jobtitle, location, startdate, enddate, description], (err, result) => {
+        if (err) return res.status(500).json({error: "Unexpected " + err});
+
+        res.json({message: "Experience added", experience: result.rows[0]});
+    });
+    }
+});
+
+// Update in database -- ANVÄNDS INTE
+app.put("/api/experience", (req, res) => {
+    const {id, companyname, jobtitle, location, startdate, enddate, description} = req.body;
+
+    // Error handling
+    if (!companyname || !jobtitle || !location || !startdate) {
+        return res.status(400).json({
+            message: "Missing required input.",
+            details: "Company name, job title, location and start date must be included.",
+        });
+    }
+
+    // Update work experience
+    client.query(`
+        UPDATE workexperience
+        SET (companyname, jobtitle, location, startdate, enddate, description) =
+        ROW($1, $2, $3, $4, $5, $6)
+        WHERE id = $7
+    `, [companyname, jobtitle, location, startdate, enddate, description, id], (err, result) => {
+        if (err) return res.status(500).json({error: "Something went wrong: " + err});
+
+        res.json(result.rows[0]);
+    });
+});
+
+// Delete from database
+app.delete("/api/experience/:id", (req, res) => {
+    client.query("DELETE FROM workexperience WHERE id=$1", [req.params.id], (err, result) => {
+        if (err) return res.status(500).json({error: "Unexpected " + err});
+
+        if (result.rowCount === 0) {
+            return res.status(404).json({error: "Experience not found for ID: " + req.params.id});
+        }
+
+        res.json({message: "Experience deleted: " + req.params.id});
+    });
+});
+
+// Start server
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port: " + process.env.PORT);
+});
